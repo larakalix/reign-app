498f0a2e9b51f3c6435b8444fbd4f471d2076d22
Ivan Lara <uki@live.co.uk>
2021-10-30 16:54:32 -0600

    Add head tag for site info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Head from "next/head";
 import Header from "../components/Header";
 import Board from "../components/news/Board";
 
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>News App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       <Header title="Hacker News" />
 
       <Board />
