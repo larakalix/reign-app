5479e65795c2f5879cc1babb063a37bdc4ed50c0
Ivan Lara <uki@live.co.uk>
2021-10-30 16:55:14 -0600

    Fix empty message size for mobile

--- components/general/Empty.tsx
+++ components/general/Empty.tsx
@@ -5,7 +5,7 @@ interface Props {
 const Empty = ({ message }: Props) => {
     return (
         <div className="flex justify-center items-center my-10">
-            <div className="flex justify-center items-center h-60 w-3/4 bg-white text-red-600 border border-red-200 font-semibold rounded-md">
+            <div className="flex justify-center items-center h-60 w-4/5 md:w-3/4 bg-white text-red-600 border border-red-200 font-semibold rounded-md">
                 <p>{message}</p>
             </div>
         </div>
