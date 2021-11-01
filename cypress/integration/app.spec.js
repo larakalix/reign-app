40364a8aba0e899d5499ad8418d9ef1fa3cbd61c
Ivan Lara <uki@live.co.uk>
2021-11-01 12:39:41 -0600

    Fix header in order to set into same board container width

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -5,7 +5,7 @@ context("Home Page", () => {
     cy.visit("http://localhost:3000/");
   });
 
-  it("Should find our welcome page and message", () => {
+  it("Should find app title", () => {
     cy.get("h1").contains("Hacker News");
   });
 });
